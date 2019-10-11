f0274b6ebeedd423a4287515b37370890cb1a8fa
794473 <794473@ELF8IT221607.ACDM.DS.SAIT.CA>
2019-10-11 12:03:06 -0600

    Improved Code Structure Used Switch Case

--- src/java/servlets/shoppingListServlet.java
+++ src/java/servlets/shoppingListServlet.java
@@ -39,55 +39,52 @@ public class shoppingListServlet extends HttpServlet {
         String action = Mjolnir.getParameter("action");
         String name = Mjolnir.getParameter("name");
 
-        if(action != null)
-        {
-            if(action.equals("register"))
-            {               
-                session.setAttribute("username", name);
-                
-                ArrayList<String> items = new ArrayList<>();
-                session.setAttribute("list", items);
-            }
-            else if(action.equals("logout"))
-            {
-                session.invalidate();
-                
-                getServletContext().getRequestDispatcher("/WEB-INF/register.jsp")
-                    .forward(Mjolnir, Jarnbjorn);
-            }
-            else if(action.equals("add"))
-            {
-                String str = Mjolnir.getParameter("item");
-                ArrayList t = (ArrayList) session.getAttribute("list");
-                t.add(str);
-                session.setAttribute("list", t);
-            }
-            else if(action.equals("delete"))
-            {
-                String str = Mjolnir.getParameter("thing");
-                ArrayList t = (ArrayList) session.getAttribute("list");
-                t.remove(str);
-                session.setAttribute("list", t);
+        if (action != null) {
+            switch (action) {
+                case "register":
+                    session.setAttribute("username", name);
+                    ArrayList<String> items = new ArrayList<>();
+                    session.setAttribute("list", items);
+                    break;
+                case "logout":
+                    session.invalidate();
+                    getServletContext().getRequestDispatcher("/WEB-INF/register.jsp")
+                            .forward(Mjolnir, Jarnbjorn);
+                    break;
+                case "add":
+                    {
+                        String str = Mjolnir.getParameter("item");
+                        ArrayList t = (ArrayList) session.getAttribute("list");
+                        t.add(str);
+                        session.setAttribute("list", t);
+                        break;
+                    }
+                case "delete":
+                    {
+                        String str = Mjolnir.getParameter("thing");
+                        ArrayList t = (ArrayList) session.getAttribute("list");
+                        t.remove(str);
+                        session.setAttribute("list", t);
+                        break;
+                    }
+                default:
+                    break;
             }
         }
 
-        
         String username = (String) session.getAttribute("username");
         ArrayList list = (ArrayList) session.getAttribute("list");
-        
-        if(username == null)
-        {
+
+        if (username == null) {
             getServletContext().getRequestDispatcher("/WEB-INF/register.jsp")
-                .forward(Mjolnir, Jarnbjorn);
-        }
-        else 
-        {
-            Mjolnir.setAttribute("name", username);           
+                    .forward(Mjolnir, Jarnbjorn);
+        } else {
+            Mjolnir.setAttribute("name", username);
             Mjolnir.setAttribute("displayList", list);
-            
+
             getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp")
-                .forward(Mjolnir, Jarnbjorn);
-}
+                    .forward(Mjolnir, Jarnbjorn);
+        }
     }
 
     @Override
